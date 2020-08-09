c17f3d6614bfa522eb0467f074972a71c3e5e762
kh <kitt.hsu@gmail.com>
2020-08-09 22:53:52 +0800

    fixup! [TBS]-            "each of [string \"a\" case-sensitively,any character,string \"b\" case-sensitively,any character,string \"c\" case-sensitively,] in order", -    {"(a)|b", "alt{ca®p{lit{a}}lit{b}}"}, -    {"[α-ε☺]", "cc{0x3b1-0x3b5 0x263a}"}, // utf-8 -    {"[\\x{100}\\x{101}]", "litfold{Ā}"}, -    {"[Δδ]", "litfold{Δ}"},

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -111,7 +111,7 @@ public class RegexTrackTest {
     put("a|^", new TrackInfo[]{
             new TrackInfo(0, 3, "any of [string \"a\" case-sensitively,line start,]"),
             new TrackInfo(0, 1, "string \"a\" case-sensitively"),
-            new TrackInfo(1, 2, "any"),
+            new TrackInfo(1, 2, "alternative"),
             new TrackInfo(3, 3, "line start"),
     });
   }};
