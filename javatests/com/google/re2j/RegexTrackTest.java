4459dfb3b07cff594673560c60840dc8ec6b2cf7
kh <kitt.hsu@gmail.com>
2020-08-09 21:55:16 +0800

    fixup! [TBS]-            "each of [string \"a\" case-sensitively,any character,string \"b\" case-sensitively,any character,string \"c\" case-sensitively,] in order", -    {"(a)|b", "alt{ca®p{lit{a}}lit{b}}"}, -    {"[α-ε☺]", "cc{0x3b1-0x3b5 0x263a}"}, // utf-8 -    {"[\\x{100}\\x{101}]", "litfold{Ā}"}, -    {"[Δδ]", "litfold{Δ}"},

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -613,9 +613,9 @@ public class RegexTrackTest {
       ArrayList<TrackInfo> tracks = re.GetTracks();
       assertEquals(regexp, testTracks.length, tracks.size());
       for (int i = 0; i < testTracks.length; i++) {
-        assertEquals(regexp, testTracks[i].Start, tracks.get(i-1).Start);
-        assertEquals(regexp, testTracks[i].End, tracks.get(i-1).End);
-        assertEquals(regexp, testTracks[i].Info, tracks.get(i-1).Info);
+        assertEquals(regexp, testTracks[i].Start, tracks.get(i).Start);
+        assertEquals(regexp, testTracks[i].End, tracks.get(i).End);
+        assertEquals(regexp, testTracks[i].Info, tracks.get(i).Info);
       }
     }
   }
