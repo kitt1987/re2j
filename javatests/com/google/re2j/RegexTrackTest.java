63dafb4fed0fc33272de1071af3b93d01e901d99
kh <kitt.hsu@gmail.com>
2020-08-10 19:50:41 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Regexp.java#80

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -184,7 +184,7 @@ public class RegexTrackTest {
             new TrackInfo(1, 7, "repeat twice to 3 times(Perl extension: non-greedy)"),
     });
     put("a{2,}?", new TrackInfo[]{
-            new TrackInfo(0, 6, "twice to 3 times repetition of string \"a\" case-sensitively"),
+            new TrackInfo(0, 6, "twice at least repetition of string \"a\" case-sensitively"),
             new TrackInfo(0, 1, "string \"a\" case-sensitively"),
             new TrackInfo(1, 6, "repeat twice to 3 times(Perl extension: non-greedy)"),
     });
