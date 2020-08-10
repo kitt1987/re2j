070653da7b45c76894017abfe33f53bf6d37b14d
kh <kitt.hsu@gmail.com>
2020-08-11 01:04:53 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Regexp.java#80

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -305,7 +305,7 @@ public class RegexTrackTest {
             new TrackInfo(0, 11, "any character in the group [a-z]"),
     });
     put("[a-z]", new TrackInfo[]{
-            new TrackInfo(0, 11, "any character in the group [a-z]"),
+            new TrackInfo(0, 5, "any character in the group [a-z]"),
     });
   }};
 
