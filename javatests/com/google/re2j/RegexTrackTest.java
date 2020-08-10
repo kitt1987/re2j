e4ef9309187d5ee9e71e16dab6062db0b4ecfb4e
kh <kitt.hsu@gmail.com>
2020-08-10 19:51:52 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Regexp.java#80

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -190,8 +190,6 @@ public class RegexTrackTest {
     });
     put("x{1001", new TrackInfo[]{
             new TrackInfo(0, 6, "twice at least repetition of string \"a\" case-sensitively"),
-            new TrackInfo(0, 1, "string \"a\" case-sensitively"),
-            new TrackInfo(1, 6, "repeat twice at least(Perl extension: non-greedy)"),
     });
   }};
 
