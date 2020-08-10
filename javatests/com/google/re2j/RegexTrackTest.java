5db5616e2a55d06e2b49a44fac12d2fa7be6a423
kh <kitt.hsu@gmail.com>
2020-08-10 11:26:55 +0800

    fixup! [TBS]-            "each of [string \"a\" case-sensitively,any character,string \"b\" case-sensitively,any character,string \"c\" case-sensitively,] in order", -    {"(a)|b", "alt{ca®p{lit{a}}lit{b}}"}, -    {"[α-ε☺]", "cc{0x3b1-0x3b5 0x263a}"}, // utf-8 -    {"[\\x{100}\\x{101}]", "litfold{Ā}"}, -    {"[Δδ]", "litfold{Δ}"},

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -119,7 +119,7 @@ public class RegexTrackTest {
     });
     put("(a)", new TrackInfo[]{
             new TrackInfo(0, 3, "any character in the group [ab]"),
-            new TrackInfo(1, 2, "string \"a\" case-sensitively"),
+            new TrackInfo(1, 2, "Submatch of string \"a\" case-sensitively"),
     });
   }};
 
