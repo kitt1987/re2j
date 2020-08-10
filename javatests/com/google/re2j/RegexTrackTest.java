31f41fdca352070d4e966526884db30061035634
kh <kitt.hsu@gmail.com>
2020-08-10 19:43:33 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Regexp.java#80

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -176,7 +176,7 @@ public class RegexTrackTest {
     put("a{2}?", new TrackInfo[]{
             new TrackInfo(0, 5, "twice repetition of string \"a\" case-sensitively"),
             new TrackInfo(0, 1, "string \"a\" case-sensitively"),
-            new TrackInfo(1, 5, "repeat zero or once(Perl extension: non-greedy)"),
+            new TrackInfo(1, 5, "repeat twice(Perl extension: non-greedy)"),
     });
   }};
 
