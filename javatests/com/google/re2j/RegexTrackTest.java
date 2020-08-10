cdfc83a2924050b0a890a27ec63fa9861256ff39
kh <kitt.hsu@gmail.com>
2020-08-10 19:34:22 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Regexp.java#80

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -176,9 +176,7 @@ public class RegexTrackTest {
     put("a{2}?", new TrackInfo[]{
             new TrackInfo(0, 5, "0 or 1 repetition of string \"a\" case-sensitively"),
             new TrackInfo(0, 1, "string \"a\" case-sensitively"),
-            new TrackInfo(1, 5, "repeat zero or once(Perl extension: non-greedy)"),
-            new TrackInfo(2, 3, "repeat zero or once(Perl extension: non-greedy)"),
-            new TrackInfo(4, 5, "repeat zero or once(Perl extension: non-greedy)"),
+            new TrackInfo(1, 4, "repeat zero or once(Perl extension: non-greedy)"),
     });
   }};
 
