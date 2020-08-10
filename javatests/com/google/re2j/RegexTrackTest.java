cb83a5a8d89c1b31e2ab58c78b3bd8ed07950b12
kh <kitt.hsu@gmail.com>
2020-08-11 01:02:02 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Regexp.java#80

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -293,10 +293,17 @@ public class RegexTrackTest {
             new TrackInfo(3, 4, "alternative"),
             new TrackInfo(4, 7, "string \"def\" case-sensitively"),
     });
+    put("abc|def|ghi", new TrackInfo[]{
+            new TrackInfo(0, 11, "any of [string \"abc\" case-sensitively,string \"def\" case-sensitively,string \"ghi\" case-sensitively,]"),
+            new TrackInfo(0, 3, "string \"abc\" case-sensitively"),
+            new TrackInfo(3, 4, "alternative"),
+            new TrackInfo(4, 7, "string \"def\" case-sensitively"),
+            new TrackInfo(7, 8, "alternative"),
+            new TrackInfo(8, 11, "string \"ghi\" case-sensitively"),
+    });
   }};
 
 //  {
-//    {"abc|def", "alt{str{abc}str{def}}"},
 //    {"abc|def|ghi", "alt{str{abc}str{def}str{ghi}}"},
 //
 //    // Posix and Perl extensions
