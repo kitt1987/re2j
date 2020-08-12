42e948a65e332cb7c7ed5bae83054e2e85531cc0
kh <kitt.hsu@gmail.com>
2020-08-12 15:00:00 +0800

    fixup! [TBS]trivial changes in vatests/com/google/re2j/RegexTrackTest.java#2

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -104,7 +104,9 @@ public class RegexTrackTest {
 //                new Track(4, 5, "string \"c\" case-sensitively"),
 //        });
         put("abc", new Track[]{
-                new Track(0, 3, "string \"abc\""),
+                new Track(0, 1, "literal 'a'"),
+                new Track(1, 2, "literal 'b'"),
+                new Track(2, 3, "literal 'c'"),
         });
 //        put("a|^", new Track[]{
 //                new Track(0, 3, "any of [string \"a\" case-sensitively,line start,]"),
