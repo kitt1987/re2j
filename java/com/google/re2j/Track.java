ad44538fdd0f4924f73c3e0a70e5628b685581d3
kh <kitt.hsu@gmail.com>
2020-08-12 15:46:04 +0800

    fixup! [TBS]trivial changes in vatests/com/google/re2j/RegexTrackTest.java#2

--- java/com/google/re2j/Track.java
+++ java/com/google/re2j/Track.java
@@ -17,6 +17,26 @@ public class Track {
     private int flag;
     private boolean frozen;
 
+    static void FilterOnlyLiteral(ArrayList<Track> sortedLiterals) {
+        int lastLiteralPos = 0;
+        for (int i = 0; i < sortedLiterals.size(); i++) {
+            Track e = sortedLiterals.get(i);
+            if (e.type != Type.Literal) {
+                continue;
+            }
+
+            if (i != lastLiteralPos) {
+                sortedLiterals.set(lastLiteralPos, e);
+            }
+
+            lastLiteralPos++;
+        }
+
+        for (; lastLiteralPos < sortedLiterals.size(); lastLiteralPos++) {
+            sortedLiterals.remove(sortedLiterals);
+        }
+    }
+
     static Track ConcatLiteralOrString(ArrayList<Track> sortedLiterals) {
         if (sortedLiterals.size() == 1) {
             return sortedLiterals.get(0);
