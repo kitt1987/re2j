52fb237bb56b215cf90e902d502aa5096d28f3f0
kh <kitt.hsu@gmail.com>
2020-08-15 22:04:11 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Parser.java#15

--- java/com/google/re2j/Parser.java
+++ java/com/google/re2j/Parser.java
@@ -1143,6 +1143,8 @@ class Parser {
 
     // Non-capturing group.  Might also twiddle Perl flags.
     t.skip(2); // "(?"
+    // √ Save the flag track
+    t.PushNewTrack("group name \"" + name + "\"");
     int flags = this.flags;
     int sign = +1;
     boolean sawFlag = false;
