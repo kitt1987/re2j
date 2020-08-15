c250b889e39f8a81459561e719312e69d35b5e4c
kh <kitt.hsu@gmail.com>
2020-08-15 22:12:18 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Parser.java#15

--- java/com/google/re2j/Parser.java
+++ java/com/google/re2j/Parser.java
@@ -1164,7 +1164,7 @@ class Parser {
         case 'm':
           flags &= ~RE2.ONE_LINE;
           sawFlag = true;
-          t.PushNewTrack("multiline");
+          t.PushNewTrack("single line");
           break;
         case 's':
           flags |= RE2.DOT_NL;
