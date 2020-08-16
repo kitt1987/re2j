93d395b9a6b1641c8fc43b90dffad2b2a17b76b2
kh <kitt.hsu@gmail.com>
2020-08-16 16:54:16 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Track.java#85

--- java/com/google/re2j/Track.java
+++ java/com/google/re2j/Track.java
@@ -162,25 +162,25 @@ public class Track {
             case STAR:
                 b.append(joinComments(re.subs)).append(" repeated zero or many times");
                 if ((re.flags & RE2.NON_GREEDY) != 0) {
-                    b.append("non-greedy");
+                    b.append("(non-greedy)");
                 }
                 break;
             case PLUS:
                 b.append(joinComments(re.subs)).append(" repeated once or many times");
                 if ((re.flags & RE2.NON_GREEDY) != 0) {
-                    b.append("non-greedy");
+                    b.append("(non-greedy)");
                 }
                 break;
             case QUEST:
                 b.append(joinComments(re.subs)).append(" repeated zero or once");
                 if ((re.flags & RE2.NON_GREEDY) != 0) {
-                    b.append("non-greedy");
+                    b.append("(non-greedy)");
                 }
                 break;
             case REPEAT:
                 b.append(joinComments(re.subs)).append(" ").append(GenRepeatedRangeComments(re.min, re.max));
                 if ((re.flags & RE2.NON_GREEDY) != 0) {
-                    b.append("non-greedy");
+                    b.append("(non-greedy)");
                 }
                 break;
         }
