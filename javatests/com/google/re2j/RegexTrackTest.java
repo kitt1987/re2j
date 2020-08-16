f4c29ebc5053c31e8129841aa37e9a5f9c85f113
kh <kitt.hsu@gmail.com>
2020-08-16 16:57:26 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Track.java#85

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -186,11 +186,12 @@ public class RegexTrackTest {
                 new Track(1, 4, "quantifier: repeated twice"),
                 new Track(4, 5, "quantifier: non-greedy"),
         });
-//        put("a{2,3}?", new Track[]{
-//                new Track(0, 7, "twice to 3 times repetition of string \"a\" case-sensitively"),
-//                new Track(0, 1, "string \"a\" case-sensitively"),
-//                new Track(1, 7, "repeat twice to 3 times(Perl extension: non-greedy)"),
-//        });
+        put("a{2,3}?", new Track[]{
+                new Track(0, 7, "literal 'a' repeated twice to 3 times(non-greedy)"),
+                new Track(0, 1, "literal 'a'"),
+                new Track(1, 6, "quantifier: repeated twice to 3 times"),
+                new Track(6, 7, "quantifier: non-greedy"),
+        });
 //        put("a{2,}?", new Track[]{
 //                new Track(0, 6, "twice at least repetition of string \"a\" case-sensitively"),
 //                new Track(0, 1, "string \"a\" case-sensitively"),
