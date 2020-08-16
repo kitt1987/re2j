5469bf75d9185d18cc6a7a3916f3da1787592da9
kh <kitt.hsu@gmail.com>
2020-08-16 23:37:25 +0800

    fixup! [TBS]trivial changes in vatests/com/google/re2j/RegexTrackTest.java#21

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -330,6 +330,13 @@ public class RegexTrackTest {
                 new Track(10, 11, "character class end"),
         });
         // FIXME the negative sign
+        put("[^[:lower:]]", new Track[]{
+                new Track(0, 11, "character class of [POSIX class lowercase letters]"),
+                new Track(0, 1, "character class"),
+                new Track(1, 10, "POSIX class lowercase letters"),
+                new Track(10, 11, "character class end"),
+        });
+
 //    {"[^[:lower:]]", "cc{0x0-0x60 0x7b-0x10ffff}"},
 //    {"[[:^lower:]]", "cc{0x0-0x60 0x7b-0x10ffff}"},
 //    put("[^[:lower:]]", new Track[]{
