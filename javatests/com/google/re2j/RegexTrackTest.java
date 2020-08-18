11949d6701fbb7cdce74131188007a0938f1429a
kh <kitt.hsu@gmail.com>
2020-08-19 06:04:57 +0800

    fixup! [TBS]-                new Track(5, 7, "string \"ab\" repeated zero or many times"),

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -542,13 +542,13 @@ public class RegexTrackTest {
         });
 
         //    {"(?:a)", "lit{a}"},
-        put("(?:a)", new Track[]{
+//        put("(?:a)", new Track[]{
                 // FIXME fixes
-                new Track(4, 5, "literal 'a'"),
-                new Track(0, 2, "non-capturing group"),
-                new Track(2, 3, "mod modifier end"),
-                new Track(3, 4, ""),
-        });
+//                new Track(4, 5, "literal 'a'"),
+//                new Track(0, 2, "non-capturing group"),
+//                new Track(2, 3, "mod modifier end"),
+//                new Track(3, 4, ""),
+//        });
     }};
 
 //  {
