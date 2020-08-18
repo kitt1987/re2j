5e3c3843e18820511f55799ba74744bcb87a1511
kh <kitt.hsu@gmail.com>
2020-08-18 12:13:16 +0800

    fixup! the topmost track

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -517,12 +517,12 @@ public class RegexTrackTest {
 
         put("(ab)*", new Track[]{
                 // FIXME the topmost track
-                new Track(5, 7, "string \"ab\" repeated zero or many times"),
-                new Track(5, 6, "string \"ab\""),
-                new Track(0, 2, "non-capturing group"),
-                new Track(2, 3, "mod modifier end"),
-                new Track(3, 4, "literal '{'"),
-                new Track(6, 7, "quantifier: repeated zero or many times"),
+                new Track(0, 5, "capturing group (string \"ab\") repeated zero or many times"),
+                new Track(0, 4, "capturing group (string \"ab\")"),
+                new Track(0, 1, "capturing group"),
+                new Track(1, 3, "string \"ab\""),
+                new Track(3, 4, "capturing group end"),
+                new Track(4, 5, "quantifier: repeated zero or many times"),
         });
     }};
 
