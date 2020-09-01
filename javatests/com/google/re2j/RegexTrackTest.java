ebfeaeaffd4f4a7da9ae2d3cb9aed3437c3dfb10
kh <kitt.hsu@gmail.com>
2020-09-01 09:31:08 +0800

    fixup! [TBS]-        // If one of Regexps is an escaped literal, it would has a composed track and two track w/ an escape sign in it -            throw new IllegalStateException("literal track should have no composed track but " + composedTracks.size()); -            throw new IllegalStateException("literal track should have only 1 track but " + tracks.size()); -            throw new IllegalStateException("literal track should have no composed track but " + that.composedTracks.size()); -            throw new IllegalStateException("literal track should have only 1 track but " + that.tracks.size());

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -1079,7 +1079,7 @@ public class RegexTrackTest {
 
     @Test
     public void testToStringEquivalentParse() throws PatternSyntaxException {
-        testRegexpTrack("\\.\\^\\$\\\\");
+        testRegexpTrack(".^");
 
         for (String regexp : PARSE_TESTS.keySet()) {
             testRegexpTrack(regexp);
