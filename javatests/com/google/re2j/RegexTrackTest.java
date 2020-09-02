47d26f5b508443ce54e3c63927f8968d2341f9f3
kh <kitt.hsu@gmail.com>
2020-09-02 17:09:55 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Track.java -                new Track(0, 6, "character class of [case insensitive,character class word character shorthand]"),

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -553,9 +553,9 @@ public class RegexTrackTest {
         });
 
         put("(?:a)", new Track[]{
-                new Track(0, 5, "group of literal 'a'"),
+                new Track(0, 5, "literal 'a'"),
                 new Track(0, 3, "non-capturing group"),
-                new Track(0, 2, "non-capturing group"),
+                new Track(0, 2, "non-capturing group start"),
                 new Track(2, 3, "mod modifier end"),
                 new Track(3, 4, "literal 'a'"),
                 new Track(4, 5, "capturing group end"),
