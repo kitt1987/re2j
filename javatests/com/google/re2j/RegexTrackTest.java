94bbc73f9d9ceea07e93ddc55f2f6a6f73865241
kh <kitt.hsu@gmail.com>
2020-09-02 10:36:18 +0800

    fixup! [TBS]-                new Track(0, 15, "group of alternation of [group of alternation of [literal 'a',literal 'b'],literal 'a',literal 'b',group of alternation of [literal 'c',literal 'd'],literal 'c',alternation,literal 'd']"), -                new Track(0, 7, "group of alternation of [literal 'a',literal 'b']"),

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -542,7 +542,7 @@ public class RegexTrackTest {
         });
 
         put("a(b|c)d", new Track[]{
-                new Track(0, 7, "sequence of [literal 'a',capturing group (alternation of [literal 'b',literal 'c']),literal 'd']"),
+                new Track(0, 7, "sequence of [literal 'a',capturing group of [alternation of [literal 'b',literal 'c']],literal 'd']"),
                 new Track(0, 1, "literal 'a'"),
                 new Track(1, 6, "capturing group (alternation of [literal 'b',literal 'c'])"),
                 new Track(1, 2, "capturing group"),
