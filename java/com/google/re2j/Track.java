d0c9aac09d6cfdc100068c09550949570f15b809
kh <kitt.hsu@gmail.com>
2020-09-02 23:31:51 +0800

    fixup! [TBS]trivial changes in va/com/google/re2j/Parser.java trivial changes in vatests/com/google/re2j/RegexTrackTest.java

--- java/com/google/re2j/Track.java
+++ java/com/google/re2j/Track.java
@@ -55,6 +55,7 @@ public class Track implements Comparable<Track>  {
         CommentMap.put("[:xdigit:]", "hexadecimal digits");
         CommentMap.put("[:^xdigit:]", "negated hexadecimal digits");
         CommentMap.put("\\A", "word start");
+        CommentMap.put("\\z", "word end");
         CommentMap.put("\\d", "digits");
         CommentMap.put("\\D", "non-digits");
         CommentMap.put("\\s", "whitespace");
