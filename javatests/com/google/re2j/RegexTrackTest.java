5a10f67c994157b114ed027103ec485bfb3078a7
kh <kitt.hsu@gmail.com>
2020-09-03 22:21:23 +0800

    fixup! lack of tracks

--- javatests/com/google/re2j/RegexTrackTest.java
+++ javatests/com/google/re2j/RegexTrackTest.java
@@ -1436,7 +1436,7 @@ public class RegexTrackTest {
 
     @Test
     public void testToStringEquivalentParse() throws PatternSyntaxException {
-        testRegexpTrack("a(?:b)c|abd");
+        testRegexpTrack("a.*?c|a.*?b");
 
         for (String regexp : PARSE_TESTS.keySet()) {
             testRegexpTrack(regexp);
